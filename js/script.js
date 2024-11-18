1668426fbffc035c4f1ee9b4b788e09e012ab3f4
SANJANA2312 <sanjana@edu.sait.ca>
2024-11-18 10:46:30 -0700

    scoring added

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 console.log("Is this the real life?");
 
+
+const end = 20;
+let score = 0;
+
 const makeGrid = () => {
 // rows and coloumns 4*5
 console.log(`makeGrid is being called`);
 const grid = document.getElementById("grid");
-for (let i = 1; i <= 20; i++) {
+for (let i = 1; i <= end; i++) {
     
     const button = document.createElement("button");
     button.innerText =  i;
@@ -15,12 +19,26 @@ for (let i = 1; i <= 20; i++) {
             button.innerText = "fizzbuzz";
         //button.style.backgroundColor = "#ffff00"; 
         button.className = "fizzbuzz" ;
+        score++;
     }
         if (fizzbuzz(i) == "fizz") { button.innerText = "fizz";
             button.className = "fizz" ;
+            score++;
         }
         if (fizzbuzz(i) == "buzz") { button.innerText = "buzz";
             button.className = "buzz" ;
+            score++;
+        }
+        /*
+        if (fizzbuzz(i) != "fizz" || fizzbuzz(i) != "buzz" || fizzbuzz(i) !="fizzbuzz") {
+            score--;
+        } else {
+            score++;
+        } */
+        button.disabled = true;
+        console.log(score);
+        if (score >= 9) {
+            alert(`YOU WIN!`);
         }
      });
     
